a0c6284e300779f716a08df1c6c842e648dd62b2
Hexagrim <hexagrim3456@gmail.com>
2026-04-23 09:51:05 +0545

    Hour 1, made the most sophisticated player movement code with apex movement thing and coyote timing and jump biffering and more

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,128 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float movespeed;
+    public float maxSpeed;
+    public float jumpSpeed;
+
+    public KeyCode Left, Right, Up;
+
+    public float apexGravity, upGravity, downGravity;
+
+    bool isGrounded;
+
+    public Transform groundCheck;
+    public float groundCheckSize;
+    public LayerMask groundLayer;
+
+    public float apexSpeed;
+
+    private float coyoteTimeCounter;
+    private float coyoteTime = 0.15f;
+
+    private float jumpBufferTime = 0.15f;
+    private float jumpBufferCounter;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGrounded)
+        {
+            coyoteTimeCounter = coyoteTime;
+        }
+        else
+        {
+            coyoteTimeCounter -= Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(Up))
+        {
+            jumpBufferCounter = jumpBufferTime;
+        }
+        else
+        {
+            jumpBufferCounter -= Time.deltaTime;
+        }
+
+
+
+
+        if (jumpBufferCounter >0f && coyoteTimeCounter > 0f)
+        {
+            rb.linearVelocityY = jumpSpeed;
+
+            jumpBufferCounter = 0f;
+        }
+        if (Mathf.Abs(rb.linearVelocityY) <= 0.5f && !isGrounded)
+        {
+            rb.gravityScale = apexGravity;
+        }
+        else if (rb.linearVelocityY < 0f)
+        {
+            rb.gravityScale = downGravity;
+        }
+        else
+        {
+            rb.gravityScale = upGravity;
+        }
+
+        isGrounded = Physics2D.OverlapBox(groundCheck.position, Vector2.one * groundCheckSize, 0f, groundLayer);
+
+        if(isGrounded)
+        {
+            rb.linearDamping = 2f;
+        }
+        else
+        {
+            rb.linearDamping = 0f;
+        }
+
+        if(Input.GetKeyUp(Up) && rb.linearVelocityY > 0f)
+        {
+            rb.linearVelocityY *= 0.75f;
+            coyoteTimeCounter = 0f;
+        }
+
+        if(Mathf.Abs(rb.linearVelocityY) <= 0.5f && !isGrounded)
+        {
+            rb.linearVelocityX = Mathf.Clamp(rb.linearVelocityX, -apexSpeed, +apexSpeed);
+        }
+        else
+        {
+            rb.linearVelocityX = Mathf.Clamp(rb.linearVelocityX, -maxSpeed, maxSpeed);
+
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(Left))
+        {
+            if (rb.linearVelocityX > 0f)
+            {
+                rb.linearVelocityX = 0f;
+            }
+            else rb.AddForceX(-movespeed);
+        }
+        else if (Input.GetKey(Right))
+        {
+            if (rb.linearVelocityX < 0f)
+            {
+                rb.linearVelocityX = 0f;
+            }
+            else rb.AddForceX(movespeed);
+        }
+        else
+        {
+            rb.linearVelocityX *= 0.5f;
+        }
+
+    }
+}
